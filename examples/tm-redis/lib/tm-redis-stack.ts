34692542425b62740e0001c0d9a5994ecc905540
tm-lcarvalho <lucio.carvalho@toumoro.com>
2024-08-05 16:02:23 -0400

    feat(tm-redis): Example to TmElasticacheRedisCluster construct

---
+++ examples/tm-redis/lib/tm-redis-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { TmElasticacheRedisCluster } from '../../../src/databases/elasticache-redis-cluster';
+
+interface TmRedisStackProps extends cdk.StackProps {
+  vpc: ec2.IVpc;
+  globalReplicationGroupId: string;
+  allowFromConstructs?: { [key: string]: ec2.IConnectable  }; 
+}
+
+export class TmRedisStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: TmRedisStackProps) {
+    super(scope, id, props);
+
+    new TmElasticacheRedisCluster(this, 'MyRedisCluster', {
+      envName: 'dev',
+      vpc: props!.vpc,
+      globalReplicationGroupId: props!.globalReplicationGroupId,
+      allowFromConstructs: props!.allowFromConstructs,
+    });
+
+  }
+}
