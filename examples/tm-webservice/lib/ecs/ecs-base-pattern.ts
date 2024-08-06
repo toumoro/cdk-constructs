ce6adff8bfd5ea365bf0080eb78ff29f227fa975
tm_efosso <egs.fosso@toumoro.com>
2024-08-06 17:22:59 -0400

    fix(tm-webservice): Adding Env variables from ParameterStore

---
+++ examples/tm-webservice/lib/ecs/ecs-base-pattern.ts
@@ -0,0 +1,143 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Construct } from 'constructs';
+import { ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
+
+
+/**
+ * Represents the configuration for an ecsPatterns.
+ */
+
+export interface TmApplicationLoadBalancedFargateServiceProps extends ecsPatterns.ApplicationLoadBalancedFargateServiceProps {
+
+  /**
+ * The number of cpu units used by the task.
+ */
+  readonly vpc?: ec2.IVpc;
+
+  /**
+ * The number of cpu units used by the task.
+ */
+  readonly cpu?: number;
+
+  /**
+ * The amount (in MiB) of memory used by the task.
+ */
+  readonly memoryLimitMiB?: number;
+
+  /**
+ * The desired number of instantiations of the task definition to keep running on the service.
+ */
+  readonly desiredCount?: number;
+
+  /**
+* The container port.
+*/
+  readonly containerPort?: number;
+
+  /**
+* The certificate .
+*/
+  readonly certificate?: ICertificate;
+
+  /**
+* The listener port.
+*/
+  readonly listenerPort?: number;
+
+  /**
+* The minumun number od tasks.
+*/
+  readonly minTaskCount?: number;
+
+  /**
+* The maximum number of task.
+*/
+  readonly maxTaskCount?: number;
+
+    /**
+* Custom http header value.
+*/
+readonly customHttpHeaderValue?: string;
+
+
+}
+
+
+export class TmApplicationLoadBalancedFargateService extends ecsPatterns.ApplicationLoadBalancedFargateService {
+
+  constructor(scope: Construct, id: string, props: TmApplicationLoadBalancedFargateServiceProps,) {
+    const defautProps: TmApplicationLoadBalancedFargateServiceProps = {
+      vpc: props.vpc,
+      assignPublicIp: true,
+      enableExecuteCommand: true,
+      memoryLimitMiB: 2048,
+      cpu: 1024,
+      desiredCount: 2,
+      minTaskCount: 1,
+      maxTaskCount: 3,
+      listenerPort: 443,
+      openListener: false,
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      targetProtocol: elbv2.ApplicationProtocol.HTTP,
+      taskSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC
+      },
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset('lib/ecs/containerImage'),
+        containerPort: props.containerPort, // Optional: Specify the container port
+        enableLogging: true,
+        containerName: 'web',
+      }
+    };
+
+    const mergedProps = { ...defautProps, ...props };
+
+    super(scope, id, mergedProps);
+
+    // Remove the default action by setting a new default action with conditions
+    this.listener.addTargetGroups('HeaderConditionForward', {
+      priority: 1,
+      conditions: [
+        elbv2.ListenerCondition.httpHeader('X-Custom-Header', [mergedProps.customHttpHeaderValue || ''])
+      ],
+      targetGroups: [this.targetGroup],
+    });
+
+    // Add a default action to return an access denied response
+    this.listener.addAction('DefaultAction', {
+      action: elbv2.ListenerAction.fixedResponse(403, {
+        contentType: 'text/plain',
+        messageBody: 'Access Denied',
+      }),
+    });
+
+    // Configure auto-scaling
+    const scaling = this.service.autoScaleTaskCount({
+      minCapacity: mergedProps.minTaskCount,
+      maxCapacity: mergedProps.maxTaskCount || 3,
+    });
+
+    // Scale based on CPU utilization
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60),
+    });
+
+    // Scale based on Memory utilization
+    scaling.scaleOnMemoryUtilization('MemoryScaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60),
+    });
+
+ 
+  };
+
+
+}
+
