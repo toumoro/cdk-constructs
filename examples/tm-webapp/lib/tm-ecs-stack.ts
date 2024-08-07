236fd266539776e317245a2c5f60950fc81ae178
tm_efosso <egs.fosso@toumoro.com>
2024-08-07 16:35:39 -0400

    fix(tm-ecs-stack): integration de la stack ecs

--- examples/tm-webapp/lib/tm-ecs-stack.ts
+++ examples/tm-webapp/lib/tm-ecs-stack.ts
@@ -19,15 +19,16 @@ export interface TmEcsStackProps extends cdk.StackProps {
   readonly cpu?: number;
   readonly desiredCount?: number;
   readonly containerPort?: number;
-  //readonly domainName: string;
-
-  //readonly hostedZoneId: string;
+  readonly hostedZoneIdParameterName: string;
+  readonly customHttpHeaderParameterName: string;
+  readonly domainParameterName: string;
   readonly minTaskCount?: number;
   readonly maxTaskCount?: number;
-  //readonly customHttpHeaderValue?: string;
-
-  readonly buildContextPath?: string;
-  readonly buildDockerfile?: string;
+  readonly secrets_from_ssm_parameter_store?: string[];
+  readonly additional_secrets_from_parameter_store?: { [key: string]: string };
+  readonly applicationName: string;
+  readonly buildContextPath: string;
+  readonly buildDockerfile: string;
 }
 
 export class TmEcsStack extends cdk.Stack {
@@ -52,14 +53,42 @@ export class TmEcsStack extends cdk.Stack {
       description: 'ALB Security Group',
     });
 
-    const customHttpHeaderValue = ssm.StringParameter.valueForStringParameter(
-      this, 'customHttpHeaderValue');
-    const domainName = ssm.StringParameter.valueForStringParameter(
-      this, 'domainName');
-    const hostedZoneId = ssm.StringParameter.valueForStringParameter(
-      this, 'hostedZoneId');
-
     lbSecurityGroup.addIngressRule(ec2.Peer.prefixList(cloudFrontPrefixListId), ec2.Port.tcp(443), 'Allow HTTPS from CloudFront');
+
+    // const customHttpHeaderValue = ssm.StringParameter.valueForStringParameter(
+    //   this, 'customHttpHeaderValue');
+    // const domainName = ssm.StringParameter.valueForStringParameter(
+    //   this, 'domainName');
+    // const hostedZoneId = ssm.StringParameter.valueForStringParameter(
+    //   this, 'hostedZoneId');
+
+    // Image config
+    const secrets_from_ssm_parameter_store: string[] = props.secrets_from_ssm_parameter_store || [];
+    const additional_secrets_from_ssm_parameter_store: { [key: string]: string } = props.additional_secrets_from_parameter_store || {};
+
+    const environment_secrets: { [key: string]: ecs.Secret } = {};
+
+    this.addEnvironmentSecrets(secrets_from_ssm_parameter_store, environment_secrets, props.applicationName);
+    this.addAdditionalEnvironmentSecrets(additional_secrets_from_ssm_parameter_store, environment_secrets);
+
+    // /** Service Props*/
+    // const patternsProps: TmApplicationLoadBalancedFargateServiceProps = {
+    //   vpc: props.vpc,
+    //   memoryLimitMiB: props.memoryLimitMiB,
+    //   cpu: props.cpu,
+    //   desiredCount: props.desiredCount,
+    //   minTaskCount: props.minTaskCount,
+    //   maxTaskCount: props.maxTaskCount,
+    //   containerPort: props.containerPort,
+    //   //customHttpHeaderValue: props.customHttpHeaderValue,
+    //   customHttpHeaderValue: customHttpHeaderValue,
+    //   buildContextPath: props.buildContextPath ?? './',
+    //   buildDockerfile: props.buildDockerfile ?? 'Dockerfile',
+    //   certificate: new acm.Certificate(this, 'Certificate', {
+    //     domainName: domainName,
+    //     validation: acm.CertificateValidation.fromDns(HostedZone.fromHostedZoneId(this, 'HostedZone', hostedZoneId)),
+    //   }),
+    // }
 
     /** Service Props*/
     const patternsProps: TmApplicationLoadBalancedFargateServiceProps = {
@@ -70,14 +99,14 @@ export class TmEcsStack extends cdk.Stack {
       minTaskCount: props.minTaskCount,
       maxTaskCount: props.maxTaskCount,
       containerPort: props.containerPort,
-      //customHttpHeaderValue: props.customHttpHeaderValue,
-      customHttpHeaderValue: customHttpHeaderValue,
-      buildContextPath: props.buildContextPath ?? './',
-      buildDockerfile: props.buildDockerfile ?? 'Dockerfile',
+      secrets: environment_secrets,
+      customHttpHeaderValue: ssm.StringParameter.valueForStringParameter(this, props.customHttpHeaderParameterName),
       certificate: new acm.Certificate(this, 'Certificate', {
-        domainName: domainName,
-        validation: acm.CertificateValidation.fromDns(HostedZone.fromHostedZoneId(this, 'HostedZone', hostedZoneId)),
+        domainName: ssm.StringParameter.valueForStringParameter(this, props.domainParameterName),
+        validation: acm.CertificateValidation.fromDns(HostedZone.fromHostedZoneId(this, 'HostedZone', ssm.StringParameter.valueForStringParameter(this, props.hostedZoneIdParameterName))),
       }),
+      buildContextPath: props.buildContextPath,
+      buildDockerfile: props.buildDockerfile,
     }
 
     /** Service Pattern */
@@ -87,18 +116,39 @@ export class TmEcsStack extends cdk.Stack {
     tmPatterns.taskDefinition.addToExecutionRolePolicy(new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       actions: [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
       ],
       resources: ["*"],
-  }));
+    }));
 
     this.loadbalancer = tmPatterns.loadBalancer;
     this.cluster = tmPatterns.cluster;
     this.fargateService = tmPatterns.service;
+  }
+
+
+  private addEnvironmentSecrets(secrets: string[], environmentSecrets: { [key: string]: ecs.Secret }, applicationName: string) {
+    for (const secret of secrets) {
+      const secretParameter = ssm.StringParameter.fromSecureStringParameterAttributes(this, `${secret}SSMParameter`, {
+        parameterName: `/applications/${applicationName}/secrets/${secret}`,
+      });
+      environmentSecrets[secret] = ecs.Secret.fromSsmParameter(secretParameter);
+    }
+  }
+
+  private addAdditionalEnvironmentSecrets(secrets: { [key: string]: string }, environmentSecrets: { [key: string]: ecs.Secret }) {
+    Object.entries(secrets).forEach(([key, value]) => {
+      const secretParameter = ssm.StringParameter.fromSecureStringParameterAttributes(this, `${key}SSMParameter`, {
+        parameterName: `${value}`
+      });
+      environmentSecrets[`${key}`] = ecs.Secret.fromSsmParameter(secretParameter);
+    })
+
+
   }
 }
