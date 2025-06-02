9dd9d4a54d16ce9ff58f544ebb1170da7dc044ab
tm-lcarvalho <lucio.carvalho@toumoro.com>
2025-06-02 15:54:05 -0400

    feat(tm-solr-ec2): Add example of solr ec2 ansible stack

---
+++ examples/tm-solr-ec2-ansible/test/tm-solr-ec2-ansible.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TmSolrEc2Ansible from '../lib/tm-solr-ec2-ansible-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tm-solr-ec2-ansible-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TmSolrEc2Ansible.TmSolrEc2AnsibleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
