e54b048a1a2b33eff987e5db8191a5daa61de50a
tm-efosso <ega.fosso@toumoro.com>
2026-01-14 09:37:37 -0500

    fix(TmRdsAuroraMysqlDashboard): set custom dashboard name

--- src/databases/rds-aurora-mysql-dashboard.ts
+++ src/databases/rds-aurora-mysql-dashboard.ts
@@ -1,67 +1,61 @@
-import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as cw from 'aws-cdk-lib/aws-cloudwatch';
 import { Construct } from 'constructs';
 
-interface TmRdsAuroraMysqlDashboardProps {
-  clusterIdentifier: string;
-  dashboardName?: string;
+interface TmRdsAuroraMysqlDashboardProps extends cw.DashboardProps {
+    clusterIdentifier: string;
 }
 
-export class TmRdsAuroraMysqlDashboard extends Construct {
-  constructor(scope: Construct, id: string, props: TmRdsAuroraMysqlDashboardProps) {
-    super(scope, id);
+export class TmRdsAuroraMysqlDashboard extends cw.Dashboard {
+    constructor(scope: Construct, id: string, props: TmRdsAuroraMysqlDashboardProps) {
+        super(scope, id, props);
 
-    const logGroupName = `/aws/rds/cluster/${props.clusterIdentifier}/slowquery`;
+        const logGroupName = `/aws/rds/cluster/${props.clusterIdentifier}/slowquery`;
 
-    // Dashboard creation
-    const dashboard = new cloudwatch.Dashboard(this, 'RdsAuroraMysqlDashboard', {
-      dashboardName: props.dashboardName || 'MysqlDashboard',
-    });
-
-    // 2. Widget : Top 20 slowest queries (Table)
-    const slowQueryList = new cloudwatch.LogQueryWidget({
-      title: "Top 20 Slowest Queries",
-      logGroupNames: [logGroupName],
-      view: cloudwatch.LogQueryVisualizationType.TABLE,
-      width: 24,
-      height: 8,
-      queryString: `
-      fields @timestamp
-      | filter @message like /Query_time:/
-      | parse @message "# Query_time: * Lock_time: * Rows_sent: * Rows_examined: *" 
-             as query_time, lock_time, rows_sent, rows_examined
-      | sort query_time desc
-      | limit 20 
-      `,
-    });
+        // 1. Widget : Top 20 slowest queries (Table)
+        const slowQueryList = new cw.LogQueryWidget({
+            title: "Top 20 Slowest Queries",
+            logGroupNames: [logGroupName],
+            view: cw.LogQueryVisualizationType.TABLE,
+            width: 24,
+            height: 8,
+            queryString: `
+        fields @timestamp
+        | filter @message like /Query_time:/
+        | parse @message "# Query_time: * Lock_time: * Rows_sent: * Rows_examined: *" 
+               as query_time, lock_time, rows_sent, rows_examined
+        | sort query_time desc
+        | limit 20 
+        `,
+        });
 
 
-    // 3. Widget : average response time (Graphic)
-    const latencyOverTime = new cloudwatch.LogQueryWidget({
-      title: "Average Latency (seconds) over time",
-      logGroupNames: [logGroupName],
-      view: cloudwatch.LogQueryVisualizationType.LINE,
-      width: 12,
-      height: 6,
-      queryString: `
-      fields @timestamp, @message
-      | parse @message "Query_time: * " as duration
-      | stats avg(duration) by bin(5m)
-      `,
-    });
+        // 2. Widget : average response time (Graphic)
+        const latencyOverTime = new cw.LogQueryWidget({
+            title: "Average Latency (seconds) over time",
+            logGroupNames: [logGroupName],
+            view: cw.LogQueryVisualizationType.LINE,
+            width: 12,
+            height: 6,
+            queryString: `
+        fields @timestamp, @message
+        | parse @message "Query_time: * " as duration
+        | stats avg(duration) by bin(5m)
+        `,
+        });
 
-    // 4. Widget : slow queries volume per minute
-    const volumeWidget = new cloudwatch.LogQueryWidget({
-      title: "Slow Query Count per minute",
-      logGroupNames: [logGroupName],
-      view: cloudwatch.LogQueryVisualizationType.BAR,
-      width: 12,
-      height: 6,
-      queryString: `
-        stats count(*) by bin(1m)
-      `,
-    });
+        // 3. Widget : slow queries volume per minute
+        const volumeWidget = new cw.LogQueryWidget({
+            title: "Slow Query Count per minute",
+            logGroupNames: [logGroupName],
+            view: cw.LogQueryVisualizationType.BAR,
+            width: 12,
+            height: 6,
+            queryString: `
+          stats count(*) by bin(1m)
+        `,
+        });
 
-    dashboard.addWidgets(latencyOverTime, volumeWidget);
-    dashboard.addWidgets(slowQueryList);
-  }
+        this.addWidgets(latencyOverTime, volumeWidget);
+        this.addWidgets(slowQueryList);
+    }
 }
